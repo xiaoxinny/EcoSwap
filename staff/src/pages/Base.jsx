d9aae8fc7837bfdfa9562f6fd0aed99606b52f13
xiaoxinny <yijiaxin2004@gmail.com>
2024-07-09 16:13:35 +0800

    Implemented resizing, created README.md and cleaned up files

--- staff/src/pages/Base.jsx
+++ staff/src/pages/Base.jsx
@@ -1,43 +1,74 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
-import '../styles/Base.css';
+import "../styles/Base.css";
+import { Navbar } from "react-bootstrap";
 
-function Layout(){
+function Layout() {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 768px)").matches
+  )
+
+  useEffect(() => {
+    window
+    .matchMedia("(min-width: 768px)")
+    .addEventListener('change', e => setMatches( e.matches ));
+  }, []);
+
   return (
     <>
       <div>
+      {matches && (
         {/* Top Bar */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-white shadow p-3 mb-5 bg-body-tertiary rounded fixed-top" style={{ height: "56px" }}>
-        <Link className="navbar-brand text-dark fs-2" to="/">EcoSwap</Link>
-          <div className="container">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-          </div>
-        </nav>
+        <Navbar
+          className=" navbar-light bg-white shadow p-3 mb-5 bg-body-tertiary rounded fixed-top"
+          style={{ height: "56px" }}
+        >
+          <Link className="navbar-brand text-dark fs-2" to="/">
+            EcoSwap
+          </Link>
+        </Navbar>
 
-        <div className="container-fluid ">
-          <div className="row">
-            {/* Side Navbar */}
-            <div className="col-2 bg-dark" style={{ marginTop: '56px', minHeight: '100vh', position: 'fixed' }}>
-              <nav className="nav flex-column">
-                <h3 className="text-light pt-4">Users</h3>
-                <Link className="nav-link text-light" to="/">Account Management</Link>
-                <Link className="nav-link text-light" to="/">Violations</Link>
+        <div className="container-fluid">
+          
+            <div className="row">
+              {/* Side Navbar */}
+              <div
+                className="col-2 bg-dark"
+                style={{
+                  marginTop: "56px",
+                  minHeight: "100vh",
+                  position: "fixed",
+                }}
+              >
+                <nav className="nav flex-column">
+                  <h3 className="text-light pt-4">Users</h3>
+                  <Link className="nav-link text-light" to="/">
+                    Account Management
+                  </Link>
+                  <Link className="nav-link text-light" to="/">
+                    Violations
+                  </Link>
 
-                <h3 className="text-light pt-4">Staff</h3>
-                <Link className="nav-link text-light" to="/FAQ">FAQs Management</Link>
-                <Link className="nav-link text-light" to="/">Listings Management</Link>
-                <Link className="nav-link text-light" to="/">Event Management</Link>
-              </nav>
+                  <h3 className="text-light pt-4">Staff</h3>
+                  <Link className="nav-link text-light" to="/FAQ">
+                    FAQs Management
+                  </Link>
+                  <Link className="nav-link text-light" to="/">
+                    Listings Management
+                  </Link>
+                  <Link className="nav-link text-light" to="/">
+                    Event Management
+                  </Link>
+                </nav>
+              </div>
+
+              {/* Scrolling */}
+              <div className="col-10 offset-2" style={{ marginTop: "56px" }}>
+                <Outlet />
+              </div>
             </div>
-
-            {/* Scrolling */}
-            <div className="col-10 offset-2" style={{ marginTop: '56px' }}>
-              <Outlet />
-            </div>
-          </div>
         </div>
+        )}
       </div>
     </>
   );
